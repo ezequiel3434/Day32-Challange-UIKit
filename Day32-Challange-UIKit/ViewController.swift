71b3dda651a6da3125a7f6d6ad1c7780c85465eb
ezequiel3434 <ezequiel3434@gmail.com>
2020-08-17 20:16:12 -0300

    add and refresh list functions done

--- Day32-Challange-UIKit/ViewController.swift
+++ Day32-Challange-UIKit/ViewController.swift
@@ -8,13 +8,60 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
 
+    let key = "list"
+    let defaults = UserDefaults.standard
+    var shoppingList = [String]()
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .refresh, target: self, action: #selector(clearList))
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addItem))
+        
+    }
+    
+    // Clear list
+    
+    @objc func clearList() {
+        shoppingList.removeAll(keepingCapacity: true)
+        tableView.reloadData()
+    }
+    @objc func addItem(){
+        let ac = UIAlertController(title: "Enter a item", message: nil, preferredStyle: .alert)
+        ac.addTextField()
+        let submitAction = UIAlertAction(title: "Submit", style: .default) {
+            [weak self, weak ac] _ in
+            guard let answer = ac?.textFields?[0].text else {
+                return
+            }
+            self?.submit(answer)
+            
+        }
+        ac.addAction(submitAction)
+        present(ac, animated: true)
+    }
+    
+    
+    func submit(_ answer: String) {
+        let loweAnswer = answer.lowercased()
+        shoppingList.insert(loweAnswer, at: 0)
+        let indexPath = IndexPath(row: 0, section: 0)
+        tableView.insertRows(at: [indexPath], with: .automatic)
     }
 
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return shoppingList.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Item", for: indexPath)
+        cell.textLabel?.text = shoppingList[indexPath.row]
+        return cell
+    }
 
 }
 
