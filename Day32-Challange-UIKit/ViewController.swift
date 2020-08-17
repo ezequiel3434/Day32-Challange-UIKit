297118464d626561461c094c3dc0339162da2ea9
ezequiel3434 <ezequiel3434@gmail.com>
2020-08-17 19:15:45 -0300

    Initial Commit

---
+++ Day32-Challange-UIKit/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Day32-Challange-UIKit
+//
+//  Created by Ezequiel Parada Beltran on 17/08/2020.
+//  Copyright © 2020 Ezequiel Parada. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
